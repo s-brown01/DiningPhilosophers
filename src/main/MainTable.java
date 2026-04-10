2c37f40fae8247ddedcdcadbedf20c14babaaa7d
s-brown01 <sbrown01@carroll.edu>
2026-04-09 18:45:28 -0600

    Adding Javadoc to the MainTable

--- src/main/MainTable.java
+++ src/main/MainTable.java
@@ -1,37 +1,91 @@
 package main;
 
+/**
+ * Controller for the Dining Philosophers simulation.
+ *
+ * <p>This class is responsible for creating and running the simulation.
+ * It creates all required {@link Chopstick} and {@link Philosopher} Objects,
+ * starts the philosophers, lets them execute for the desired time, then shuts them down safely.
+ * </p>
+ *
+ * <p>This simulation demonstrates a solution to the classic Dining Philosophers concurrency problem.
+ * Here, multiple threads compete for shared resources, which can lead to issues such as deadlock and starvation.
+ * </p>
+ *
+ * @author Sean-Paul Brown
+ */
 public class MainTable {
     
     /**
-     * How many philosophers to use
+     * The default number of philosophers to use
      */
     private static final int NO_OF_PHILOSOPHER = 5;
     /**
-     * How long the simulation will run in milliseconds. 1000 ms per 1 second * 10 for 10 total seconds
+     * Default duration of the simulation in milliseconds.
+     * <p>Calculated as 1000 ms (as 1 second) * 10 for 10 total seconds</p>
      */
     private static final int SIMULATION_MILLIS = 1000 * 10;
     
     /**
-     * The main method to run everything
-     * @param args
+     * Main method and entry point for simulation.
+     *
+     * <p>Creates a {@link MainTable} instance with default configuration and starts simulation</p>
+     *
+     * @param args <b>UNUSED</b>
      */
     public static void main(String[] args) {
         MainTable mainTable = new MainTable();
         mainTable.start();
     }
     
+    /**
+     * The number of philosophers and chopsticks in the simulation
+     */
     private final int numPhil;
+    /**
+     * Duration of the simulation in milliseconds
+     */
     private final int timeToRunMS;
     
+    /**
+     * Constructor with the default values
+     */
     MainTable() {
         this(NO_OF_PHILOSOPHER, SIMULATION_MILLIS);
     }
     
+    /**
+     * Constructor with custom values
+     *
+     * @param numberOfPhilosophers the number of philosophers to simulate
+     * @param timeToRunMS the duration of the simulation in milliseconds
+     */
     MainTable(int numberOfPhilosophers, int timeToRunMS) {
         this.numPhil = numberOfPhilosophers;
         this.timeToRunMS = timeToRunMS;
     }
     
+    /**
+     * Starts and manages the simulation of Dining Philosophers
+     *
+     * <p>
+     *     <ol>
+     *         <li>Initializes an array of {@link Chopstick} Objects.</li>
+     *         <li>Initializes an array of {@link Philosopher} Objects, assigning each a left and right chopstick</li>
+     *         <li>Starts all philosopher threads</li>
+     *         <li>Waits for the desired duration</li>
+     *         <li>Signals all philosophers to stop</li>
+     *         <li>Waits for all philosophers to stop</li>
+     *         <li>Prints a summary of the simulation</li>
+     *     </ol>
+     * </p>
+     *
+     * <p>
+     *     Chopsticks are assigned in a way that the last philosopher's right chopstick
+     *     is the same chopstick as the first philosopher's left.
+     *     This mirrors the classic problem setup.
+     * </p>
+     */
     public void start() {
         // make the chopsticks array and initialize it
         final Chopstick[] chopsticks = new Chopstick[numPhil];
